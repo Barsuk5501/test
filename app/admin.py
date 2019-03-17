5de092237c26bd8d2f6c6648ae4f9a244f705c33
Ваше Имя <you@example.com>
2019-03-17 15:09:38 +0200

    strink

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import MyUser
+from .models import Admin
+from .models import Post
 
+admin.site.register(Admin)
 admin.site.register(MyUser)
+admin.site.register(Post)
 
 
